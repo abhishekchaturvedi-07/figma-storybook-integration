290308eaca32cb7142c5c316b5ea735041b54fb8
abhishekchaturvedi-07 <abhishekchaturvedi789@gmail.com>
2023-10-11 20:49:34 +0530

    Figma Properties and Storybook Integration

--- src/stories/FigmaDesign.stories.ts
+++ src/stories/FigmaDesign.stories.ts
@@ -8,7 +8,7 @@ export default {
             type: 'figspec',
             // url: "https://www.figma.com/file/QqXWSdBmrFTj2Hd0bojWleYv/Figma-Embed-Example?type=design&node-id=0-21&mode=design&t=gIfsIxAsTnLhuW8A-4",
             url: 'https://www.figma.com/file/QqXWSdBmrFTj2Hd0bojWleYv/Figma-Embed-Example?type=design&node-id=0-2&mode=design&t=gIfsIxAsTnLhuW8A-4',
-            accessToken: "figd_Ed9KcaovNlBk84QaJ9a3PV_aki_OOzpTwrJAFRkK",
+            accessToken: '<CAN BE ACCESS FROM HERE>',
         }
     }
 };
